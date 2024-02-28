73d8c1d5bf743325b3e15358439416799d289b41
Yanlin(Eric) Zhu <ylinzhu19@gmail.com>
2024-02-27 23:05:04 -0500

    Remove extra id

--- src/features/search/ImagesList.jsx
+++ src/features/search/ImagesList.jsx
@@ -66,7 +66,6 @@ export default function ImagesList() {
 					return (
 						<Col key={image.id} xxl={4} lg={6} md={8} sm={12} xs={12}>
 							<LazyLoadImage
-								key={image.id}
 								height={200}
 								width="100%"
 								src={link}
